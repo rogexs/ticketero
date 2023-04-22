7ea78b93e6791808460ed416f73eb1a24cc8ce7d
Rogelio Santiago Celaya <zs20019092@estudiantes.uv.mx>
2023-04-21 21:10:42 -0600

    V0.4 Creando proceso de registro de evento

--- js/registro-evento.js
+++ js/registro-evento.js
@@ -1,18 +1,6 @@
 import { supabase } from "./conexion.js"
 
 const txtNombre = document.getElementById("nombre");
-const txtTipo = document.getElementById("tipo");
-const txtFecha = document.getElementById("fecha");
-const txtHorario = document.getElementById("horario");
-const txtCiudad = document.getElementById("ciudad");
-const txtDireccion = document.getElementById("direccion");
-const txtOrganizador = document.getElementById("organizador");
-const txtCantidadInvitados = document.getElementById("cantidad-invitados");
-const txtFechaLimite = document.getElementById("fecha-limite");
-const txtPrecioBoleto = document.getElementById("precio-boleto");
-const txtCostoExtra = document.getElementById("costo-extra");
-const txtPublicoPrivado = document.getElementById("publico-privado");
-const txtDescripcion = document.getElementById("descripcion");
 
 const btnRegistrar = document.getElementById("registrar");
 
@@ -20,22 +8,10 @@ btnRegistrar.addEventListener('click', registrarEvento)
 
 async function registrarEvento() {
    let nombre = txtNombre.value;
-   let tipo = txtTipo.value;
-   let fecha = txtFecha.value;
-   let horario_inicio = txtHorario.value;
-   let ciudad = txtCiudad.value;
-   let direccion = txtDireccion.value;
-   let organizador = txtOrganizador.value;
-   let cantidad_invitados = txtCantidadInvitados.value;
-   let fecha_limite = txtFechaLimite.value;
-   let precio_boleto = txtPrecioBoleto.value;
-   let costo_extra = txtCostoExtra.value;
-   let publico_privado = txtPublicoPrivado.value;
-   let descripcion = txtDescripcion.value;
 
    const { error } = await supabase
       .from('eventos')
-      .insert({ nombre:'convencion', tipo:'convencion', fecha, horario_inicio, ciudad, direccion, organizador, cantidad_invitados, fecha_limite , precio_boleto, costo_extra, publico_privado, descripcion })
+      .insert({ nombre })
 
    if (error) {
       alert('Registro evento incorrecto')
