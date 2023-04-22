f1c8d21af680a8a1dba36e09fc2b82cc26e24109
Rogelio Santiago Celaya <zs20019092@estudiantes.uv.mx>
2023-04-22 17:41:37 -0600

    V0.4 Registro estable

--- README.md
+++ README.md
@@ -2,3 +2,6 @@
 Implementados los casos de uso:
  -Registro
  -Inicio de sesion.
+
+ Registro de evento se encuentra en trabajo, aun no se pueden cargar imagenes
+ De los casos de inicio de sesion y registro de usuarios y eventos aun se busca que se vinculen por id a la tabla de usuarios autenticados
--- js/registro-evento.js
+++ js/registro-evento.js
@@ -1,6 +1,19 @@
 import { supabase } from "./conexion.js"
 
 const txtNombre = document.getElementById("nombre");
+const txtTipo = document.getElementById("tipo");
+const txtFecha = document.getElementById("fecha");
+const txtHorarioInicio = document.getElementById("horarioInicio");
+const txtHorarioFinal = document.getElementById("horarioFinal");
+const txtCiudad = document.getElementById("ciudad");
+const txtDireccion = document.getElementById("direccion");
+const txtOrganizador = document.getElementById("organizador");
+const txtCantidadInvitados = document.getElementById("cantidad-invitados");
+const txtFechaLimite = document.getElementById("fecha-limite");
+const txtPrecioBoleto = document.getElementById("precio-boleto");
+const txtCostoExtra = document.getElementById("costo-extra");
+const txtPublicoPrivado = document.getElementById("publico-privado");
+const txtDescripcion = document.getElementById("descripcion");
 
 const btnRegistrar = document.getElementById("registrar");
 
@@ -8,13 +21,27 @@ btnRegistrar.addEventListener('click', registrarEvento)
 
 async function registrarEvento() {
    let nombre = txtNombre.value;
+   let tipo = txtTipo.value;
+   let fecha = txtFecha.value;
+   let horario_inicio = txtHorarioInicio.value;
+   let horario_final = txtHorarioFinal.value;
+   let ciudad = txtCiudad.value;
+   let direccion = txtDireccion.value;
+   let organizador = txtOrganizador.value;
+   let cantidad_invitados = txtCantidadInvitados.value;
+   let fecha_limite = txtFechaLimite.value;
+   let precio_boleto = txtPrecioBoleto.value;
+   let costo_extra = txtCostoExtra.value;
+   let publico_privado = txtPublicoPrivado.value;
+   let descripcion = txtDescripcion.value;
 
    const { error } = await supabase
       .from('eventos')
-      .insert({ nombre })
+      .insert({ nombre, tipo, fecha, horario_inicio, horario_final, ciudad, direccion, organizador, cantidad_invitados, fecha_limite, precio_boleto, costo_extra, publico_privado, descripcion })
 
    if (error) {
       alert('Registro evento incorrecto')
+      console.log(error);
    } else {
       alert('Registro evento correcto');
    }
