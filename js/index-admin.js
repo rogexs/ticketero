5436ea140190f691de595a587788e16acdc48fab
Rogelio Santiago Celaya <zs20019092@estudiantes.uv.mx>
2023-04-24 11:14:37 -0600

    V0.10 Visualizando datos mediante boton

--- js/index-admin.js
+++ js/index-admin.js
@@ -6,38 +6,50 @@ btnAccion.addEventListener('click', accionFuncion)
 
 async function accionFuncion() {
 
-    const { data, error } = await supabase
+    let { data: { user } } = await supabase.auth.getUser()
+    console.log(user.id);
+    
+    let { data, error } = await supabase
+        .from('eventos')
+        .select(`count`)
+        .eq('id_usuario_auth', user.id)
+
+    console.log(data[0].count);
+    let limite = data[0].count;
+
+    ({ data, error } = await supabase
         .from('eventos')
         .select()
-        .eq('id_usuario_auth', 'a35419a1-7ce1-4182-ae46-23168d30d6d6')
+        .eq('id_usuario_auth', 'a35419a1-7ce1-4182-ae46-23168d30d6d6'))
 
     console.log(data[0]);
 
-    /**
-    var nuevoDiv = document.createElement("div");
+    for (let index = 0; index < limite; index++) {
+
+        var nuevoDiv = document.createElement("div");
 
     // Establecer el estilo del nuevo div
     nuevoDiv.setAttribute("style", "border: 1rem solid goldenrod;");
 
     // Composicion del div
     var nombreEvento = document.createElement("p");
-    var textoNombreEvento = document.createTextNode(data[0].nombre);
+    var textoNombreEvento = document.createTextNode(data[index].nombre);
     nombreEvento.appendChild(textoNombreEvento);
 
     var fecha = document.createElement("p");
-    var textoFecha = document.createTextNode("Fecha: "+data[0].fecha);
+    var textoFecha = document.createTextNode("Fecha: "+data[index].fecha);
     fecha.appendChild(textoFecha);
 
     var entrada = document.createElement("p");
-    var textoEntrada = document.createTextNode("Entrada: "+data[0].precio_boleto);
+    var textoEntrada = document.createTextNode("Entrada: "+data[index].precio_boleto);
     entrada.appendChild(textoEntrada);
 
     var estado = document.createElement("p");
-    var textoEstado = document.createTextNode("Estado: "+data[0].estado);
+    var textoEstado = document.createTextNode("Estado: "+data[index].estado);
     estado.appendChild(textoEstado);
 
     var tipoEvento = document.createElement("p");
-    var textoTipoEvento = document.createTextNode("Tipo de evento: "+data[0].publico_privado);
+    var textoTipoEvento = document.createTextNode("Tipo de evento: "+data[index].publico_privado);
     tipoEvento.appendChild(textoTipoEvento);
 
     // Agregar los elementos <p> al nuevo div
@@ -50,5 +62,7 @@ async function accionFuncion() {
     // Agregar el nuevo div al contenedor
     var contenedor = document.getElementById("contenedor");
     contenedor.appendChild(nuevoDiv);
-     */
+
+    }
+
 }
