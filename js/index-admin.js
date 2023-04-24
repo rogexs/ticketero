8207b92cbfd47fe1e675d88ffc310b896062d253
Rogelio Santiago Celaya <zs20019092@estudiantes.uv.mx>
2023-04-24 09:43:49 -0600

    V0.9 Datos de un evento

--- js/index-admin.js
+++ js/index-admin.js
@@ -11,32 +11,33 @@ async function accionFuncion() {
         .select()
         .eq('id_usuario_auth', 'a35419a1-7ce1-4182-ae46-23168d30d6d6')
 
-    console.log(data[0].id_eventos);
+    console.log(data[0]);
 
+    /**
     var nuevoDiv = document.createElement("div");
 
     // Establecer el estilo del nuevo div
     nuevoDiv.setAttribute("style", "border: 1rem solid goldenrod;");
 
-    // Crear los elementos <p> y establecer su contenido
+    // Composicion del div
     var nombreEvento = document.createElement("p");
-    var textoNombreEvento = document.createTextNode("Nombre de evento");
+    var textoNombreEvento = document.createTextNode(data[0].nombre);
     nombreEvento.appendChild(textoNombreEvento);
 
     var fecha = document.createElement("p");
-    var textoFecha = document.createTextNode("Fecha: xx/xx/xxxx");
+    var textoFecha = document.createTextNode("Fecha: "+data[0].fecha);
     fecha.appendChild(textoFecha);
 
     var entrada = document.createElement("p");
-    var textoEntrada = document.createTextNode("Entrada: $$$$$");
+    var textoEntrada = document.createTextNode("Entrada: "+data[0].precio_boleto);
     entrada.appendChild(textoEntrada);
 
     var estado = document.createElement("p");
-    var textoEstado = document.createTextNode("Estado: xxxxxxx");
+    var textoEstado = document.createTextNode("Estado: "+data[0].estado);
     estado.appendChild(textoEstado);
 
     var tipoEvento = document.createElement("p");
-    var textoTipoEvento = document.createTextNode("Tipo de evento: xxxxxxx");
+    var textoTipoEvento = document.createTextNode("Tipo de evento: "+data[0].publico_privado);
     tipoEvento.appendChild(textoTipoEvento);
 
     // Agregar los elementos <p> al nuevo div
@@ -49,4 +50,5 @@ async function accionFuncion() {
     // Agregar el nuevo div al contenedor
     var contenedor = document.getElementById("contenedor");
     contenedor.appendChild(nuevoDiv);
+     */
 }
