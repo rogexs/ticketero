85705f77cee8fd243f5f82b0eaacdb289af3c069
Oscar Daniel Villegas Martinez <villegas.martinez.oscar.cb85@gmail.com>
2023-04-25 16:38:17 -0600

    Ventanas de admin casi completas

--- prueba/main.js
+++ prueba/main.js
@@ -1,3 +1,17 @@
-import './app/firebase.js'
+import { onAuthStateChanged } from "https://www.gstatic.com/firebasejs/9.19.0/firebase-auth.js"
 
-console.log('Hello world');
+import { auth } from './firebase.js'
+import {loginCheck} from './loginCheck.js'
+
+import './signupForm.js'
+import './logout.js'
+
+onAuthStateChanged(auth, async (user) =>{
+   
+    loginCheck(user)
+    
+})
+
+
+
+
