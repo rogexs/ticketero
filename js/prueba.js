27e1ec30ae21ac9539be9da819adf253d67e0cb3
Rogelio Santiago Celaya <zs20019092@estudiantes.uv.mx>
2023-05-02 11:03:17 -0600

    V0.17 Metodo de obtencion de link de imagen

---
+++ js/prueba.js
@@ -0,0 +1,15 @@
+import { supabase } from "./conexion.js"
+
+/*Metodo para obtener link de imagen
+
+async function accionFuncion() {
+
+    const { data, error } = await supabase
+        .storage
+        .from('imagen-evento')
+        .createSignedUrl('40', 600000)
+
+    console.log(data.signedUrl);
+    console.log(error);
+}
+*/
